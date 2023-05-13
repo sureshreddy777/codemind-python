4d8cacdd8bff1bbbbdae03585b917e1b3a4da596
sureshreddy777 <119723601+sureshreddy777@users.noreply.github.com>
2023-05-13 19:33:01 +0530

    t=input() h,m=map(int,t.split(':')) ha=(h%12)*30+m*0.5 ma=m*6 ad=abs(ha-ma) m=min(ad,360-ad) print(m)

--- Angle_Between_Hands.py
+++ Angle_Between_Hands.py
@@ -1,13 +1,7 @@
-t = input()
-
-h, m = map(int, t.split(':'))
-
-ha = (h % 12) * 30 + m * 0.5
-
-ma = m* 6
-
-ad = abs(ha - ma)
-
-m = min(ad, 360-ad)
-
+t=input()
+h,m=map(int,t.split(':'))
+ha=(h%12)*30+m*0.5
+ma=m*6
+ad=abs(ha-ma)
+m=min(ad,360-ad)
 print(m)
